60f9056f77ed8f6b6700cb09b385c4e4ea59c6b5
Peter Ballok <pballok@gmail.com>
2010-04-08 17:16:57 +0200

    added 'workdays' table

--- sql/watta.sql
+++ sql/watta.sql
@@ -1,15 +1,23 @@
 DROP TABLE IF EXISTS `logs`;
 CREATE TABLE `logs` (
-  `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-  `severity` INT(1) NOT NULL DEFAULT 5 ,
-  `message` TEXT NULL DEFAULT NULL ,
+  `date`          TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `severity`      INT(1) NOT NULL DEFAULT 5,
+  `message`       TEXT NULL DEFAULT NULL,
   INDEX `logs_date` (`date` ASC)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
+DROP TABLE IF EXISTS `workdays`;
+CREATE TABLE `workdays` (
+  `date`          DATE NOT NULL,
+  `activities`    varchar(1024),
+  PRIMARY KEY (`date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 DROP TABLE IF EXISTS `sessions`;
 CREATE TABLE `sessions` (
-  `startTime`     timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `endTime`       timestamp,
-  `activities`    varchar(1024),
-  PRIMARY KEY     (`startTime`)
+  `date`           DATE NOT NULL,
+  `startTime`     TIME NOT NULL,
+  `endTime`       TIME NOT NULL,
+  INDEX sessions_date (`date`),
+  FOREIGN KEY (`date`) REFERENCES `workdays`(`date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
